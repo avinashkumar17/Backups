dac80a396f31ec6cd0090f911d1aa9d5bdc1b3ef
avinash <avinashsivakumar17061993@gmail.com>
2019-06-25 02:23:15 +0530

    h

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit ,ViewChild,ElementRef,AfterViewInit} from '@angular/core';
 import {Router} from'@angular/router';
 import {environment } from '../../environments/environment';
+import {AppServiceService} from '../../app-service.service';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit, AfterViewInit{
-  public @ViewChild("iid",{read:ElementRef}) iid : ElementRef;
+  
   public displayedColumns:any[]=["username","mobilenum","imei","custom"];
   public dataSource:any[]=environment.data;
-  constructor(private router:Router) { }
+  constructor(private router:Router,private service: AppServiceService) { }
 
   ngOnInit() {
+    this.service.fork().subscribe(res=>{
+     console.log("success"+res);
+    },(err)=>{
+      console.log("error"+err);
+    },()=>{
+      console.log("complete");
+    });
+
   }
   ngAfterViewInit() {
-  this.iid.nativeElement.style.display = "none";
+  
   }
  public navi(){
    console.log("sda");
