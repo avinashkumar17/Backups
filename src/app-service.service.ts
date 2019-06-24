3d378950aee9da2844f2726446c10420f7c7b7b2
avinash <avinashsivakumar17061993@gmail.com>
2019-06-25 02:22:02 +0530

    h

--- src/app-service.service.ts
+++ src/app-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable,forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,11 @@ export class AppServiceService {
 
  public login():Observable<any>{
     return this.httpClient.get("https://api.myjson.com/bins/177fzx",{observe:'response'});
+  }
+
+  public fork():Observable<any[]>{
+    let service1=this.httpClient.get("https://api.myjson.comm/bins/177fz",{observe:'response'});
+    let service2=this.httpClient.get("https://api.myjson.com/bins/177fz",{observe:'response'});
+    return forkJoin([service1,service2]);
   }
 }
